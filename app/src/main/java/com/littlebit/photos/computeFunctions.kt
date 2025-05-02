46563d98d974fa9a3b7b415372fb370c5c999f5f
Manish Prajapati <Manish.Prajapati@ibm.com>
2025-05-03 01:23:18 +0530

    Update versionCode 2 "version 1.1"

--- app/src/main/java/com/littlebit/photos/computeFunctions.kt
+++ app/src/main/java/com/littlebit/photos/computeFunctions.kt
@@ -1,14 +1,14 @@
 package com.littlebit.photos
 
 import android.content.Context
-import com.google.firebase.auth.FirebaseAuth
 import com.littlebit.photos.ui.navigation.Screens
 
 fun getStartDestination(context: Context): String {
+    return Screens.HomeScreen.route
+    /*** To be added in future
     val sharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
     val isFirstTime = sharedPreferences.getBoolean("is_first_time", true)
     val isLoggedIn = FirebaseAuth.getInstance().currentUser != null
-
     return when {
         isFirstTime -> {
             // Set first-time flag to false
@@ -18,4 +18,5 @@ fun getStartDestination(context: Context): String {
         isLoggedIn -> Screens.HomeScreen.route
         else -> Screens.LoginScreen.route
     }
+    ****/
 }
